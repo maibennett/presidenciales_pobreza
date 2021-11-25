87858e77bf9ec8caf9c20f6b749b13da3e54aaa8
maibennett <mai.bennett@gmail.com>
2021-11-25 08:26:11 -0600

    Create 00_initial_analysis.R

---
+++ code/01_analisis/00_initial_analysis.R
@@ -0,0 +1,12 @@
+################################################################################
+### Title: "Análisis Presidenciales y Pobreza por Comuna - Chile 2021"
+### Author: Magdalena Bennett
+################################################################################
+
+rm(list = ls())
+cat("\014")
+
+library(tidyverse)
+library(ggplot2)
+
+
