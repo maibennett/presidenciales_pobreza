2a6a92273d7fad69ce4b6ed1fc92dcac4cf8bbee
maibennett <mai.bennett@gmail.com>
2021-11-25 08:38:21 -0600

    Update 00_initial_analysis.R

--- code/01_analisis/00_initial_analysis.R
+++ code/01_analisis/00_initial_analysis.R
@@ -9,4 +9,10 @@ cat("\014")
 library(tidyverse)
 library(ggplot2)
 
+servel <- read.csv("https://raw.githubusercontent.com/maibennett/presidenciales_pobreza/main/data/servel_county_clean.csv")
 
+casen <- read.csv("https://raw.githubusercontent.com/maibennett/presidenciales_pobreza/main/data/casen_county_clean.csv")
+
+casen <- casen %>% rename(comuna_id = comuna)
+
+d <- left_join(servel, casen, by = "comuna_id")
